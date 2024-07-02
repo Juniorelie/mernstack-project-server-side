a82e56652e103ee5f28d791f33519b0bdd296fb6
Kaiser_Elie <mbakopjuniorelie@gmail.com>
2024-07-02 17:04:26 +0200

    routing updated for the places

--- routes/places.routes.js
+++ routes/places.routes.js
@@ -3,26 +3,48 @@ const express = require("express");
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-        id: "p1",
-        title: "Empire State Building",
-        description: "One of the most famous sky scrapers in the world",
-        location: {
-            lat: 40.7484445,
-            lng: -73.9882393
-        },
-        address: "20 W 34th St., New York, NY 10001, États-Unis",
-        creator: "u1"
-    }
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9882393,
+    },
+    address: "20 W 34th St., New York, NY 10001, États-Unis",
+    creator: "u1",
+  },
 ];
 
 router.get("/:pid", (req, res, next) => {
-    // console.log("GET Request in Places");
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id = placeId;
-    });
-    res.json({place});
+  // console.log("GET Request in Places");
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "The provided id doesn't match any place" });
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "The provided userId doesn't match any place" });
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
