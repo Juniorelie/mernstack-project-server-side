370b36b5a9e3b6ca44a79b3ec4e726a248b56da7
Kaiser_Elie <mbakopjuniorelie@gmail.com>
2024-07-09 09:46:16 +0200

    update post route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,101 +1,104 @@
-const router = require("express").Router()
-const Post = require("../models/post.model")
-const isAuth = require("../middlewares/isAuthenticated")
-const isAdmin = require("../middlewares/isAdmin")
-const { isValidObjectId } = require("mongoose")
-const fileUploader = require("../config/cloudinaryConfig")
+const router = require("express").Router();
+const Post = require("../models/post.model");
+const isAuth = require("../middlewares/isAuthenticated");
+const isAdmin = require("../middlewares/isAdmin");
+const { isValidObjectId } = require("mongoose");
+const fileUploader = require("../config/cloudinaryConfig");
 
 // Get all posts
 
 router.get("/", isAuth, async (req, res, next) => {
-	try {
-        const posts = await Post.find().populate("userId");
-        res.json(posts);
-      } catch (error) {
-        return next(error);
-      }
-})
+  try {
+    const posts = await Post.find().populate("userId");
+    res.json(posts);
+  } catch (error) {
+    return next(error);
+  }
+});
 
 // Get post by id
 
 router.get("/:postId", async (req, res, next) => {
-	const { postId } = req.params
-	if (!isValidObjectId(postId)) {
-		console.log("Not a valid objectid", postId)
-		return next()
-	}
-	const onePost = await Post.findOne({ _id: postId }).populate("userId")
-	res.json({ post: onePost})
-	try {
-	} catch (error) {
-		console.log(error)
-	}
-})
+  const { postId } = req.params;
+  if (!isValidObjectId(postId)) {
+    console.log("Not a valid objectid", postId);
+    return next();
+  }
+  const onePost = await Post.findOne({ _id: postId }).populate("userId");
+  res.json({ post: onePost });
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Create a post
 
-router.post("/", isAuth, fileUploader.single("image"), async (req, res, next) => {
+router.post(
+  "/",
+  isAuth,
+  fileUploader.single("image"),
+  async (req, res, next) => {
+    const { title, description } = req.body;
+    let image = "";
 
-		const { title, description } = req.body
-		let image = ""
+    if (req.file) {
+      image = req.file.path;
+    }
 
-		if (req.file) {
-			image = req.file.path
-		}
+    const postToCreate = { title, image, description, userId: req.userId };
 
-		const postToCreate = { title, image, description, userId: req.userId }
+    const createdPost = await Post.create(postToCreate);
 
-		const createdPost = await Pet.create(postToCreate)
-
-		res.status(201).json(createdPost)
-		try {
-		} catch (error) {
-			console.log(error)
-		}
-	}
-)
+    res.status(201).json(createdPost);
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 // Update a post
 
 router.put("/:postId", isAuth, isAdmin, async (req, res, next) => {
-	try {
-		const { image, title, description } = req.body
-		const { postId } = req.params
-		const postToUpdate = { image, title, description }
-		let updatedPost
-		// Find and update
-		if (req.isAdmin) {
-			updatedPost = await Post.findByIdAndUpdate(postId, postToUpdate, {
-				new: true,
-			})
-		} else {
-			updatedPost = await Post.findOneAndUpdate(
-				{ _id: postId, userId: req.userId },
-				postToUpdate,
-				{ new: true }
-			)
-		}
+  try {
+    const { image, title, description } = req.body;
+    const { postId } = req.params;
+    const postToUpdate = { image, title, description };
+    let updatedPost;
+    // Find and update
+    if (req.isAdmin) {
+      updatedPost = await Post.findByIdAndUpdate(postId, postToUpdate, {
+        new: true,
+      });
+    } else {
+      updatedPost = await Post.findOneAndUpdate(
+        { _id: postId, userId: req.userId },
+        postToUpdate,
+        { new: true }
+      );
+    }
 
-		if (!updatedPost) {
-			return res.status(401).json({ message: "Denied" })
-		}
+    if (!updatedPost) {
+      return res.status(401).json({ message: "Denied" });
+    }
 
-		res.status(202).json(updatedPost)
-	} catch (error) {
-		console.log(error)
-	}
-})
+    res.status(202).json(updatedPost);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Delete a post
 
 router.delete("/:postId", isAuth, async (req, res, next) => {
-	try {
-		const { postId } = req.params
-		await Post.findOneAndDelete({ _id: postId, userId: req.userId })
-		res.sendStatus(204)
-	} catch (error) {
-		console.log(error)
-	}
-})
+  try {
+    const { postId } = req.params;
+    await Post.findOneAndDelete({ _id: postId, userId: req.userId });
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
